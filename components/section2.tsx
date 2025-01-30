ac98050cc0e6ac358c54b936692a6cc917d5573d
SatyamVyas04 <vyas.satyam.04@gmail.com>
2025-01-30 11:22:15 +0530

    Enhance SectionTwo layout with additional content and adjust image width for better responsiveness

--- components/section2.tsx
+++ components/section2.tsx
@@ -8,8 +8,19 @@ export default function SectionTwo() {
         width={1000}
         height={1000}
         alt="logo"
-        className="w-[90dvw]"
+        className="w-[95dvw]"
       />
+      <div className="relative w-full p-4 text-center font-brilliant text-white sm:text-start xl:-translate-y-16">
+        <div className="flex items-center justify-center space-x-2 text-3xl font-bold sm:justify-start xl:text-6xl">
+          <span>14+</span>
+          <span className="text-primary">REGIONS</span>
+          <div className="mt-3 hidden h-2 w-full rounded-full border-2 border-white py-2 backdrop-blur-md sm:block"></div>
+        </div>
+        <p className="mt-3 text-sm uppercase tracking-wide text-gray-300 sm:text-base">
+          Serving across the globe with a network of{' '}
+          <span className="font-bold text-white">banks</span>
+        </p>
+      </div>
     </section>
   )
 }
